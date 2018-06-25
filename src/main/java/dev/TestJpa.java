2bbb207891f6d5433c70f24c3d6560e71a9e7ad1
joachim <joachim.vanoni@gmail.com>
2018-06-25 11:46:23 +0200

    commit tp 2

--- src/main/java/dev/TestJpa.java
+++ src/main/java/dev/TestJpa.java
@@ -3,6 +3,7 @@ package dev;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import entities.Livre;
 
@@ -13,7 +14,9 @@ public class TestJpa {
 		EntityManagerFactory entityMangerFactory = Persistence.createEntityManagerFactory("jpa_test");
 		EntityManager em = entityMangerFactory.createEntityManager();
 
-		Livre livre = em.find(Livre.class, 2);
+		//Livre livre = em.find(Livre.class, 2);
+		TypedQuery<Livre> result = em.createQuery("SELECT l FROM Livre l WHERE l.title='Germinal'",Livre.class);
+		Livre livre = result.getResultList().get(0);
 		
 		System.out.println(livre);
 		
